91278f36139661138329e704b7f4ee8e04faaed9
Manuel2708 <carbricmanu@gmail.com>
2025-08-07 11:03:27 -0400

    Add files via upload

---
+++ suma de pares.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class SumaPares
+{
+    public static void Main(string[] args)
+    {
+        CalcularSumaPares();
+    }
+
+        <summary>
+
+        </summary>
+    public static void CalcularSumaPares()
+    {
+        int suma = 0; 
+
+        for (int i = 2; i <= 100; i += 2)
+        {
+            suma = suma + i; 
+        }
+
+        Console.WriteLine($"La suma de todos los números pares del 1 al 100 es: {suma}");
+    }
+}
