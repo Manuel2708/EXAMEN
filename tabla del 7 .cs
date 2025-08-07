435505ed62aa19ec59585754571c6ccbf0e070c5
Manuel2708 <carbricmanu@gmail.com>
2025-08-07 10:57:14 -0400

    Add files via upload

---
+++ tabla del 7 .cs
@@ -0,0 +1,26 @@
+using System;
+
+public class TablaMultiplicar
+{
+    public static void Main(string[] args)
+    {
+        ImprimirTablaDelSiete();
+    }
+
+        <summary>
+    
+        </summary>
+    public static void ImprimirTablaDelSiete()
+    {
+        int numero = 7;
+        
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+        Console.WriteLine("----------------------------------");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = numero * i;
+            Console.WriteLine($"{numero} x {i} = {resultado}");
+        }
+    }
+}
